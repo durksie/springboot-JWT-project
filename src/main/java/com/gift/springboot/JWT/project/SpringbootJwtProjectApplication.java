2f79aae177ae9e086c16466026f69d8622b1ed7e
Durksie <durksie1@gmail.com>
2026-02-03 14:43:36 +0200

    The first phase of the application

---
+++ src/main/java/com/gift/springboot/JWT/project/SpringbootJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gift.springboot.JWT.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gift/springboot/JWT/project/SpringbootJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gift.springboot.JWT.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
